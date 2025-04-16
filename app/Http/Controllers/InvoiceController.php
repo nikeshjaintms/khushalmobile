179c11655e6d8cae23d66fe533ac1568bf93b443
Riya Patel <riya04032002@gmail.com>
2025-04-16 11:03:12 +0530

    add imei number and color invoice pdf generated on sales module.

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -4,17 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Sale;
 use Barryvdh\DomPDF\Facade\Pdf;
-use Illuminate\Http\Request;
-use mysql_xdevapi\CollectionModify;
 
 class InvoiceController extends Controller
 {
     public function generatePDF($id)
     {
-
-       $sale = Sale::with('products','products.brand' ,'products.product','products')->findOrFail($id);
-        $pdf = Pdf::loadView('sale.invoice', compact('sale'));
-        return $pdf->stream("invoice_{$sale->id}.pdf");
-
+        $sale = Sale::with(['saleProducts', 'saleProducts.product.brand', 'saleProducts.product', 'saleProducts.purchaseProduct'])->where('id', $id)->first();
+        return Pdf::loadView('sale.invoice', compact('sale'))->stream("invoice_{$sale->id}.pdf");
     }
 }
